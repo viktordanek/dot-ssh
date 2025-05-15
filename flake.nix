ff6d9d6ab62fbdb9267a7ae141a554de71513ddb
Emory Merryman <viktordanek10@gmail.com>
2025-05-15 19:42:19 -0400

    YES

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+    inputs = { } ;
+    outputs = { self } :
+        {
+            lib.generator =
+                {
+                    nixpkgs ,
+                    system ,
+                    host ,
+                    host-name ,
+                    identity-file ,
+                    known-hosts ,
+                    port ,
+                    user
+                } :
+                    let
+                        pkgs = import nixpkgs { inherit system ; } ;
+                        in
+                            pkgs.writeShellApplication
+                                {
+                                    name = "ssh-configure" ;
+                                    runtimeInputs = [ pkgs.coreutils pkgs.openssh ] ;
+                                    text =
+                                        ''
+                                            ( cat > /mount/target <<EOF
+                                            Host ${ host }
+                                            HostName ${ host-name }
+                                            IdentityFile ${ identity-file }
+                                            Port ${ port }
+                                            User ${ user }
+                                            UserKnownHostsFile ${ known-hosts }
+                                            EOF
+                                            chmod 0400 /mount/target
+                                        '' ;
+                                } ;
+        } ;
+}
